5ee92f8680ecf19ab580ab7c8a4a6f9b66aa07fa
Julio Vargas <juliovgalvao@gmail.com>
2020-10-10 19:04:48 -0300

    Exercicios da aula de python

--- Exercicios da aula 007/exercicio_02.py
+++ Exercicios da aula 007/exercicio_02.py
@@ -1,7 +1,10 @@
 def divisao():
+
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
-    resultado = num1 / num2
-    print(f"A divisão entre os números {num1} e {num2} é: {resultado}")
-
+    if num1 == 0 or num2 == 0:
+        print("Não é possivel dividir por zero.")
+    else:
+        resultado = num1 / num2
+        print(f"A divisão entre os números {num1} e {num2} é: {resultado}")
 divisao()
